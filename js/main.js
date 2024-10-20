26c0542e051973f7a2316a94d84ef97477c41e45
David <dav.baz@alice.it>
2024-10-20 18:31:33 +0200

    update structure, and add login functionality

--- js/main.js
+++ js/main.js
@@ -5,7 +5,8 @@ import { displayResult } from './components/dialog.js';
 import { postToSteem, salvaBozza } from './page/postPage.js';
 import { showPage } from './page/page.js';
 import { openComunitiesAutocomplete, openDatePicker, togglePreview } from './page/postPage.js';
-import { enableNavigationButtons, initializeEnd } from './services/utils.js';
+import { enableNavigationButtons, initializeEnd, setUsernames } from './services/utils.js';
+import { goToSteemLogin, login, loginSteemLogin } from './page/loginPage.js';
 
 const eventListeners = [
     { id: 'goLogin', event: 'click', handler: login },
@@ -18,8 +19,8 @@ const eventListeners = [
     { id: 'draftBtn', event: 'click', handler: () => window.location.hash = '#/draft' },
     { id: 'accountBtn', event: 'click', handler: () => window.location.hash = '#/' },
     { id: 'loginInBtn', event: 'click', handler: () => window.location.hash = '#/login' },
-    { id: 'configBtn', event: 'click', handler: () => window.location.hash = '#/config' }
-
+    { id: 'configBtn', event: 'click', handler: () => window.location.hash = '#/config' },
+    { id: 'steemlogin', event: 'click', handler: goToSteemLogin }
 ];
 
 function router() {
@@ -54,126 +55,10 @@ eventListeners.forEach(({ id, event, handler }) => {
 window.listaComunities = '';
 window.scheduledTime = null;
 let client = new ApiClient();
-let usernames = [];
 window.idTelegram = '';
 window.usernameSelected = '';
 initializeImageUpload();
 
-
-function updateStatus(message) {
-    displayResult({ info: message }, 'info', true);
-}
-
-
-function handleCallback() {
-    console.log('Handling callback');
-    const urlParams = new URLSearchParams(window.location.search);
-    const accessToken = urlParams.get('access_token');
-    const state = urlParams.get('state');
-    console.log('Url params:', urlParams);
-    if (state) {
-        const savedState = sessionStorage.getItem('steemLoginState');
-        if (state === savedState) {
-            //scrivilo nel local storage
-            updateStatus('Login effettuato con successo');
-            // Rimuovi i parametri dall'URL
-            console.log("HANDLE CALLBACK", getSteemloginUsername(accessToken));
-            window.history.replaceState({}, document.title, window.location.pathname);
-        } else {
-            updateStatus('Errore: Stato non corrispondente');
-        }
-        sessionStorage.removeItem('steemLoginState');
-    }
-
-}
-
-async function getSteemloginUsername(accessToken) {
-    if (!accessToken) {
-        updateStatus('Utente non loggato. Impossibile ottenere i dati.');
-        return;
-    }
-    try {
-        const response = await fetch('https://api.steemlogin.com/api/me', {
-            headers: {
-                'Authorization': `Bearer ${accessToken}`
-            }
-        });
-        if (!response.ok) {
-            throw new Error('Errore nella richiesta API');
-        }
-        const userData = await response.json();
-        console.log('Dati utente:', userData);
-        await loginSteemLogin(userData.username);
-        displayUserData(userData);
-    } catch (error) {
-        console.error('Errore durante il recupero dei dati utente:', error);
-        updateStatus('Errore durante il recupero dei dati utente: ' + error.message);
-    }
-}
-
-function displayUserData(userData) {
-    const dialog = document.createElement('dialog');
-    dialog.classList.add('dialogo');
-    dialog.innerHTML = `
-        <h2>Dati Utente</h2>
-        <ul>
-            ${Object.entries(userData).map(([key, value]) => `<li>${key}: ${value}</li>`).join('')}
-        </ul>
-        <button id="closeButton" class="action-btn">Chiudi</button>
-    `;
-    document.body.appendChild(dialog);
-    dialog.showModal();
-    const closeButton = dialog.querySelector('#closeButton');
-    closeButton.addEventListener('click', () => {
-        dialog.remove();
-    });
-    dialog.addEventListener('close', () => dialog.remove());
-}
-
-async function loginSteemLogin(username, idTelegram) {
-    try {
-        const result = await client.login(
-            idTelegram,
-            username
-        );
-        displayResult(result, 'success', true);
-    } catch (error) {
-        console.error('Error in login:', error);
-        displayResult({ error: error.message }, 'error', true);
-    } finally {
-        document.getElementById('spinner').classList.remove('hide');
-        await client.checkLogin(idTelegram).then(async (result) => {
-            if (typeof result.usernames === 'undefined') {
-                //termina lo spinner
-                document.getElementById('spinner').classList.add('hide');
-                displayResult({ error: 'Nessun account trovato' }, 'error', true);
-                return;
-            }
-            usernames = result.usernames;
-            initializeEnd(result);
-        });
-    }
-}
-
-async function login() {
-    idTelegram = localStorage.getItem('idTelegram');
-    alert
-    try {
-        await client.login(
-            idTelegram,
-            document.getElementById('username').value,
-            document.getElementById('postingKey').value
-        );
-        await client.checkLogin(idTelegram).then(async (result) => {
-            displayResult(result, 'success', true);
-            initializeEnd(result);
-        });
-
-    } catch (error) {
-        console.error('Error in login:', error);
-        displayResult({ error: error.message }, 'error', true);
-    }
-}
 
 function showAccountPage() {
     showPage('accountPage');
@@ -211,7 +96,6 @@ function setupTelegramBackButton() {
         });
     }
 }
-
 
 // Add input event listeners to remove error class when user starts typing
 ['postTitle', 'postBody', 'postTags'].forEach(id => {
@@ -252,13 +136,12 @@ document.addEventListener('DOMContentLoaded', async () => {
                         displayResult({ error: 'Nessun account trovato' }, 'error', true);
                         return;
                     }
-                    usernames = result.usernames;
+                    setUsernames(result.usernames);
                     enableNavigationButtons();
                     initializeEnd(result);
                 } catch (error) {
                     showPage('loginPage');
                     displayResult({ error: 'Effettua il login' }, 'error', true);
-                    console.error('Error in initialize app:', error);
                     document.getElementById('spinner').classList.add('hide');
                 }
             } else {
@@ -267,7 +150,6 @@ document.addEventListener('DOMContentLoaded', async () => {
                 });
             }
         }).catch(error => {
-            console.error('Errore durante l\'inizializzazione di Telegram:', error);
             displayResult({ error: 'Errore durante l\'inizializzazione di Telegram' }, 'error', true, () => {
                 location.reload();
             });
@@ -276,25 +158,6 @@ document.addEventListener('DOMContentLoaded', async () => {
 });
 
 
-document.getElementById('steemlogin').addEventListener('click', goToSteemLogin);
 
-function goToSteemLogin() {
-    handleCallback();
-    const steemClient = new window.steemlogin.Client({
-        app: 'cur8',
-        callbackURL: window.location.origin + window.location.pathname,
-        scope: ['login', 'vote', 'comment', 'custom_json'],
-    });
 
-    try {
-        const state = Math.random().toString(36).substring(7);
-        const loginUrl = steemClient.getLoginURL(state);
-        sessionStorage.setItem('steemLoginState', state);
-        console.log('Login URL:', steemClient);
-        window.location.href = loginUrl;
-    } catch (error) {
-        console.error('Errore durante il processo di login:', error);
-        updateStatus('Errore durante il processo di login: ' + error.message);
-    }
-}
 
---
+++ js/page/loginPage.js
@@ -0,0 +1,142 @@
+
+import { displayResult } from '../components/dialog.js';
+import { setUsernames, initializeEnd } from '../services/utils.js';
+import ApiClient from '../api/api-client.js';
+
+
+const client = new ApiClient();
+
+
+function updateStatus(message) {
+    displayResult({ info: message }, 'info', true);
+}
+
+function handleCallback() {
+    console.log('Handling callback');
+    const urlParams = new URLSearchParams(window.location.search);
+    const accessToken = urlParams.get('access_token');
+    const state = urlParams.get('state');
+    console.log('Url params:', urlParams);
+    if (state) {
+        const savedState = sessionStorage.getItem('steemLoginState');
+        if (state === savedState) {
+            //scrivilo nel local storage
+            updateStatus('Login effettuato con successo');
+            // Rimuovi i parametri dall'URL
+            console.log("HANDLE CALLBACK", getSteemloginUsername(accessToken));
+            window.history.replaceState({}, document.title, window.location.pathname);
+        } else {
+            updateStatus('Errore: Stato non corrispondente');
+        }
+        sessionStorage.removeItem('steemLoginState');
+    }
+
+}
+
+async function getSteemloginUsername(accessToken) {
+    if (!accessToken) {
+        updateStatus('Utente non loggato. Impossibile ottenere i dati.');
+        return;
+    }
+    try {
+        const response = await fetch('https://api.steemlogin.com/api/me', {
+            headers: {
+                'Authorization': `Bearer ${accessToken}`
+            }
+        });
+        if (!response.ok) {
+            throw new Error('Errore nella richiesta API');
+        }
+        const userData = await response.json();
+        console.log('Dati utente:', userData);
+        await loginSteemLogin(userData.username);
+        displayUserData(userData);
+    } catch (error) {
+        console.error('Errore durante il recupero dei dati utente:', error);
+        updateStatus('Errore durante il recupero dei dati utente: ' + error.message);
+    }
+}
+
+function displayUserData(userData) {
+    const dialog = document.createElement('dialog');
+    dialog.classList.add('dialogo');
+    dialog.innerHTML = `
+        <h2>Dati Utente</h2>
+        <ul>
+            ${Object.entries(userData).map(([key, value]) => `<li>${key}: ${value}</li>`).join('')}
+        </ul>
+        <button id="closeButton" class="action-btn">Chiudi</button>
+    `;
+    document.body.appendChild(dialog);
+    dialog.showModal();
+    const closeButton = dialog.querySelector('#closeButton');
+    closeButton.addEventListener('click', () => {
+        dialog.remove();
+    });
+    dialog.addEventListener('close', () => dialog.remove());
+}
+
+export async function loginSteemLogin(username, idTelegram) {
+    try {
+        const result = await client.login(
+            idTelegram,
+            username
+        );
+        displayResult(result, 'success', true);
+    } catch (error) {
+        console.error('Error in login:', error);
+        displayResult({ error: error.message }, 'error', true);
+    } finally {
+        document.getElementById('spinner').classList.remove('hide');
+        await client.checkLogin(idTelegram).then(async (result) => {
+            if (typeof result.usernames === 'undefined') {
+                //termina lo spinner
+                document.getElementById('spinner').classList.add('hide');
+                displayResult({ error: 'Nessun account trovato' }, 'error', true);
+                return;
+            }
+            setUsernames(result.usernames);
+            initializeEnd(result);
+        });
+    }
+}
+
+export async function login() {
+    idTelegram = localStorage.getItem('idTelegram');
+    try {
+        const client = new ApiClient();
+        await client.login(
+            idTelegram,
+            document.getElementById('username').value,
+            document.getElementById('postingKey').value
+        );
+        await client.checkLogin(idTelegram).then(async (result) => {
+            displayResult(result, 'success', true);
+            initializeEnd(result);
+        });
+
+    } catch (error) {
+        console.error('Error in login:', error);
+        displayResult({ error: error.message }, 'error', true);
+    }
+}
+
+export function goToSteemLogin() {
+    handleCallback();
+    const steemClient = new window.steemlogin.Client({
+        app: 'cur8',
+        callbackURL: window.location.origin + window.location.pathname,
+        scope: ['login', 'vote', 'comment', 'custom_json'],
+    });
+
+    try {
+        const state = Math.random().toString(36).substring(7);
+        const loginUrl = steemClient.getLoginURL(state);
+        sessionStorage.setItem('steemLoginState', state);
+        console.log('Login URL:', steemClient);
+        window.location.href = loginUrl;
+    } catch (error) {
+        console.error('Errore durante il processo di login:', error);
+        updateStatus('Errore durante il processo di login: ' + error.message);
+    }
+}
