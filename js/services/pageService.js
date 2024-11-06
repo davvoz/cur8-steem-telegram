6713955c600eb1424965215b1b413e57ceda2183
tasuboyz <tasuhiro.davide@gmail.com>
2024-11-06 19:33:04 +0100

    Hide SteemLogin Button for Hive

--- js/services/pageService.js
+++ js/services/pageService.js
@@ -1,13 +1,18 @@
+const platform = localStorage.getItem('platform');
+
 export function showPage(pageId) {
     const modal = document.getElementById('previewModal');
     modal.style.display = 'none';
     document.querySelectorAll('.page').forEach(page => page.classList.remove('active'));
     document.getElementById(pageId).classList.add('active');
-    if (pageId === 'loginPage' && localStorage.getItem('platform') === 'HIVE') {
+    if (pageId === 'loginPage' && platform === 'HIVE') {
         const steemLoginElement = document.getElementById('steemLogin');
         if (steemLoginElement) {
             steemLoginElement.remove();
         }
     }
-
 }
+
+ if (platform === 'STEEM') {
+     document.getElementById('steemlogin').style.display = 'block'; 
+    }
