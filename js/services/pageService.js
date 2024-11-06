83b04f3745fcfea47365e016e8315ad4d6da161d
David <dav.baz@alice.it>
2024-11-06 22:44:52 +0100

    fix: ensure SteemLogin button visibility is correctly handled in showPage function

--- js/services/pageService.js
+++ js/services/pageService.js
@@ -1,12 +1,12 @@
 const platform = localStorage.getItem('platform');
 
 export function showPage(pageId) {
+    if (platform === 'STEEM') {
+        document.getElementById('steemlogin').style.display = 'block';
+    }
     const modal = document.getElementById('previewModal');
     modal.style.display = 'none';
     document.querySelectorAll('.page').forEach(page => page.classList.remove('active'));
     document.getElementById(pageId).classList.add('active');
-    if (platform === 'STEEM') {
-        document.getElementById('steemlogin').style.display = 'block'; 
-       }
 }
 
