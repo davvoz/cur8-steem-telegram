f9e9347a7772e4547c46816b90c20543ba0e2174
tasuboyz <tasuhiro.davide@gmail.com>
2024-11-12 22:39:51 +0100

    pageservice update

--- js/services/pageService.js
+++ js/services/pageService.js
@@ -1,9 +1,6 @@
 const platform = localStorage.getItem('platform');
 
 export function showPage(pageId) {
-    if (platform === 'STEEM') {
-        document.getElementById('steemlogin').style.display = 'block';
-    }
     const modal = document.getElementById('previewModal');
     modal.style.display = 'none';
     document.querySelectorAll('.page').forEach(page => page.classList.remove('active'));
