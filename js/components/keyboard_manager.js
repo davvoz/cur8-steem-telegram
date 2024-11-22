defec4bb2d29a70519dbe571ae2663590e924fc1
tasuboyz <tasuhiro.davide@gmail.com>
2024-11-22 22:57:43 +0100

    "Refactor keyboard manager: reorganize event listeners and add touchstart event"

--- js/components/keyboard_manager.js
+++ js/components/keyboard_manager.js
@@ -1,28 +1,35 @@
 export function setupKeyboardHandling() {
-    const inputs = document.querySelectorAll('input, textarea');
-    const keyboardDismissBtn = document.getElementById('keyboardDismiss');
-    
-    inputs.forEach(input => {
-      input.addEventListener('focus', () => {
-        if (window.innerWidth <= 768) {
-          keyboardDismissBtn.classList.add('show');
-        }
-      });
+  const inputs = document.querySelectorAll('input, textarea');
+  const keyboardDismissBtn = document.getElementById('keyboardDismiss');
   
-      input.addEventListener('blur', () => {
-        keyboardDismissBtn.classList.remove('show');
-      });
-    });
-  
-    keyboardDismissBtn.addEventListener('click', () => {
-      document.activeElement.blur();
-      keyboardDismissBtn.classList.remove('show');
-    });
-  
-    document.addEventListener('touchend', (e) => {
-      const activeElement = document.activeElement;
-      if (activeElement && activeElement.tagName === 'INPUT' && !activeElement.contains(e.target)) {
-        activeElement.blur();
+  inputs.forEach(input => {
+    input.addEventListener('focus', () => {
+      if (window.innerWidth <= 768) {
+        keyboardDismissBtn.classList.add('show');
       }
     });
-  }
+
+    input.addEventListener('blur', () => {
+      keyboardDismissBtn.classList.remove('show');
+    });
+
+    // Aggiungi un event listener per il primo tocco
+    input.addEventListener('touchstart', () => {
+      if (window.innerWidth <= 768) {
+        input.focus();
+      }
+    });
+  });
+
+  keyboardDismissBtn.addEventListener('click', () => {
+    document.activeElement.blur();
+    keyboardDismissBtn.classList.remove('show');
+  });
+
+  document.addEventListener('touchend', (e) => {
+    const activeElement = document.activeElement;
+    if (activeElement && (activeElement.tagName === 'INPUT' || activeElement.tagName === 'TEXTAREA') && !activeElement.contains(e.target)) {
+      activeElement.blur();
+    }
+  });
+}
